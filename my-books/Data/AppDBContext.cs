966e1730d34d630c38f05c778fcfd8fb81c57480
jyfernandez <jydfernandez@gmail.com>
2021-12-03 20:05:34 +0800

    Update version of .Net

---
+++ my-books/Data/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using my_books.Data.Models;
+
+namespace my_books.Data
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
