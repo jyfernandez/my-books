612467dbc021cfcae2232229754a0f2aecdb9271
jyfernandez <jydfernandez@gmail.com>
2021-12-03 20:20:10 +0800

    Update Startup and app json

--- my-books/Startup.cs
+++ my-books/Startup.cs
@@ -16,6 +16,7 @@ namespace my_books
 {
     public class Startup
     {
+        public string ConnectionString { get; set; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
